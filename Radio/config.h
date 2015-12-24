76de5609543e167d3887eab00dd868a4c490b304
hexabyte23 <glider30@yahoo.com>
2015-12-24 10:08:46 +0100

    refactoring

--- Radio/config.h
+++ Radio/config.h
@@ -1,44 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
-//#define TERRATOP                // Define the hardware you want to test
-
-#ifdef TERRATOP
-
-#define MAX_ADC_INPUT_CHANNEL   4       // Gimballs
-#define MAX_DIG_INPUT_CHANNEL   3       // Mechanical switches
-#define MAX_PPM_OUTPUT_CHANNEL  6
-#define MAX_MIXER               8
-#define PPM_SHAPE_SIGNAL        1       // 1 is positive, 0 is negative
-#define PPM_PIN                 10
-#define SWITCH1_PIN             2
-#define SWITCH2_PIN             3
-#define SWITCH3_PIN             4
-#define LED_PIN                 6
-#define BT_RX_PIN               8
-#define BT_TX_PIN               9
-#define PPM_PULSE_LEN           300     // in microseconds
-#define PPM_FRAME_LEN           22500   // in microseconds
-
-#else
-
-#define MAX_ADC_INPUT_CHANNEL   4       // Gimballs
-#define MAX_DIG_INPUT_CHANNEL   2       // Mechanical switches
-#define MAX_PPM_OUTPUT_CHANNEL  6
-#define MAX_MIXER               8
-#define PPM_SHAPE_SIGNAL        1       // 1 is positive, 0 is negative
-#define PPM_PIN                 10
-#define SWITCH1_PIN             2
-#define SWITCH2_PIN             3
-#define LED_PIN                 13
-#define BT_RX_PIN               8
-#define BT_TX_PIN               9
-#define PPM_PULSE_LEN           300     // in microseconds
-#define PPM_FRAME_LEN           22500   // in microseconds
-#endif
-
-
 // General definitions
 
 #define VERSION                 "0.0.12" 
@@ -48,7 +10,6 @@
 
 #define MAX_MODEL               2
 #define MAX_SERIAL_INPUT_BUFFER 10
-#define MAX_INPUT_CHANNEL       MAX_ADC_INPUT_CHANNEL+MAX_DIG_INPUT_CHANNEL
 
 #define ADC_MIN_VALUE           0
 #define ADC_MAX_VALUE           1023
@@ -62,5 +23,59 @@
                                         // mode 4 : left stick operates throttle & ailerons, right stick operates elevator & rudder
 
 #define LED_BLINK_PERIOD        500    // millisec
+
+
+//
+//  Put here after all definitions relative to your board configuration
+//
+
+
+//#define TERRATOP                // Define the hardware you want to test
+//#define BLUETOOTH               // uncomment if BT is wired
+
+#ifdef TERRATOP
+
+#define MAX_ADC_INPUT_CHANNEL   4       // Gimballs or pot
+#define MAX_DIG_INPUT_CHANNEL   3       // Mechanical switches
+#define MAX_PPM_OUTPUT_CHANNEL  6
+#define MAX_MIXER               8
+
+#define PPM_SHAPE_SIGNAL        1       // 1 is positive, 0 is negative
+#define PPM_PIN                 10
+#define PPM_PULSE_LEN           300     // in microseconds
+#define PPM_FRAME_LEN           22500   // in microseconds
+
+#define SWITCH1_PIN             2
+#define SWITCH2_PIN             3
+#define SWITCH3_PIN             4
+
+#define LED_PIN                 6
+
+#define BT_RX_PIN               9
+#define BT_TX_PIN               8
+
+#else
+
+#define MAX_ADC_INPUT_CHANNEL   4       // Gimballs or pot
+#define MAX_DIG_INPUT_CHANNEL   2       // Mechanical switches
+#define MAX_PPM_OUTPUT_CHANNEL  6
+#define MAX_MIXER               8
+
+#define PPM_SHAPE_SIGNAL        1       // 1 is positive, 0 is negative
+#define PPM_PIN                 10
+#define PPM_PULSE_LEN           300     // in microseconds
+#define PPM_FRAME_LEN           22500   // in microseconds
+
+#define SWITCH1_PIN             2
+#define SWITCH2_PIN             3
+
+#define LED_PIN                 13
+
+#define BT_RX_PIN               9
+#define BT_TX_PIN               8
+
+#endif
+
+#define MAX_INPUT_CHANNEL       MAX_ADC_INPUT_CHANNEL+MAX_DIG_INPUT_CHANNEL
 
 #endif
