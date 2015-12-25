10b483885f9e54d8c9b7f127ea9ef4e4b0783835
hexabyte23 <glider30@yahoo.com>
2015-12-25 16:16:11 +0100

    free static SRAM by removing extra include

--- Radio/Tx.h
+++ Radio/Tx.h
@@ -21,7 +21,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define TX_H
 
 #include <arduino.h>
-#include <SoftwareSerial.h>
 #include "config.h"
 #include "SerialLink.h"
 #include "Command.h"
