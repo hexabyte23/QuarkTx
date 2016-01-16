99f618403617af5e3911eae07bff8a2967f67f12
hexabyte23 <glider30@yahoo.com>
2016-01-16 23:05:29 +0100

    new version

--- Radio/config.h
+++ Radio/config.h
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 // General definitions
 
-#define QUARKTX_VERSION         "0.0.19" 
+#define QUARKTX_VERSION         "0.0.20"
 #define SERIAL_SPEED            9600
 
 #define MAX_MODEL               2
