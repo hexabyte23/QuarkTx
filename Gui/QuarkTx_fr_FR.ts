33c9fbe50c68d700e0cb7e7c3a1d010f5529e4ab
hexabyte23 <glider30@yahoo.com>
2016-12-31 16:21:44 +0100

    update french version

--- Gui/QuarkTx_fr_FR.ts
+++ Gui/QuarkTx_fr_FR.ts
@@ -4,15 +4,38 @@
 <context>
     <name>Antena</name>
     <message>
-        <location filename="qml/Antena.qml" line="45"/>
+        <location filename="qml/Antena.qml" line="62"/>
         <source>Tx</source>
         <translation></translation>
     </message>
 </context>
 <context>
+    <name>BTScanner</name>
+    <message>
+        <location filename="qml/BTScanner.qml" line="70"/>
+        <source>Scanning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/BTScanner.qml" line="171"/>
+        <source>Full Discovery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/BTScanner.qml" line="177"/>
+        <source>Minimal Discovery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/BTScanner.qml" line="184"/>
+        <source>Device Discovery</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Footer</name>
     <message>
-        <location filename="qml/Footer.qml" line="19"/>
+        <location filename="qml/Footer.qml" line="38"/>
         <source>Exit</source>
         <translation>Sortir</translation>
     </message>
@@ -20,9 +43,18 @@
 <context>
     <name>Header</name>
     <message>
-        <location filename="qml/Header.qml" line="35"/>
         <source>Model: </source>
-        <translation>Modèle:</translation>
+        <translation type="vanished">Modèle:</translation>
+    </message>
+    <message>
+        <location filename="qml/Header.qml" line="25"/>
+        <source>noname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qml/Header.qml" line="59"/>
+        <source> Model:</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -39,11 +71,37 @@
     </message>
 </context>
 <context>
+    <name>RclMainWindow</name>
+    <message>
+        <location filename="src/RclMainWindow.ui" line="14"/>
+        <source>RCL editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/RclMainWindow.ui" line="50"/>
+        <source>Insert Plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/RclMainWindow.ui" line="55"/>
+        <source>Save Document...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="qml/main.qml" line="11"/>
-        <source>OSRC</source>
-        <translation></translation>
+        <location filename="qml/main.qml" line="29"/>
+        <source>QuarkTx</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main2</name>
+    <message>
+        <location filename="qml/main2.qml" line="30"/>
+        <source>QuarkTx</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
